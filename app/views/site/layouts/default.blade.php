4a6bc0872d386c1c82825de0eba5454ba38afae1
Todd Vanderlin <vanderlin@bo-m-tvanderlin.local>
2015-07-08 16:24:10 -0400

    update

--- app/views/site/layouts/default.blade.php
+++ app/views/site/layouts/default.blade.php
@@ -6,28 +6,32 @@
     @yield('scripts')
   </head>
   <body class="dark-bg">
-    
-    <!-- Main Navigation -->
-    @if (isset($use_navigation)?$use_navigation:true)
-      @include('site.layouts.main-navigation')
-    @endif
-    <!-- Main Navigation -->  
+   
+    <!-- Content -->  
+    <div class="container">
+  
+      <!-- Main Navigation -->
+      @if (isset($use_navigation)?$use_navigation:true)
+        @include('site.layouts.main-navigation')
+      @endif
+      <!-- Main Navigation -->  
 
-    <!-- Content -->
-    @if (array_key_exists('content', View::getSections()))
-      <div class="container">
+
       @yield('content')
-      </div>
-    @endif
 
+
+    </div>
+  
     <!-- Retina JS -->
     <script src="{{ asset('bower_components/retinajs/dist/retina.min.js') }}"></script>
     <script src="{{asset('assets/js/classie.js')}}"></script>
     <script src="{{asset('assets/js/uiProgressButton.js')}}"></script>
 
+
     @if (isset($use_footer)?$use_footer:true)
       @include('site.layouts.footer')
     @endif
+
     
   </body>
 </html>
