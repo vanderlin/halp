2018da54e3841cbc01689ead563fcb37b0e4c99d
Todd Vanderlin <vanderlin@bo-m-tvanderlin.ideo.com>
2015-07-09 16:50:53 -0400

    run this command: art site:setup --reset=true --seed=true

--- app/views/site/tasks/card.blade.php
+++ app/views/site/tasks/card.blade.php
@@ -1,12 +1,15 @@
-<div class="task">
+<div class="task {{$claimed?'claimed':''}}">
 	<div class="task-details">
 		<span class="task-name">{{$task->title}}</span>
 		<hr>
 		<span class="project-name">For {{$task->project->title}}</span>
-		<span class="date">July 3, 2015</span>
-		<div class="progress-button small">
-			<button><span>Claim task</span></button>
-		</div>
+		<span class="date">{{$task->duration}}</span>
+		<span class="date">{{$task->created_at->toFormattedDateString()}}</span>
+		@if (!$claimed)
+			<div class="progress-button small">
+				<button><span>Claim task</span></button>
+			</div>
+		@endif
 	</div>
-	<div class="posted-by">Posted by {{$task->creator->getShortName()}}</div>
+	<div class="{{$claimed?'claimed':'posted'}}-by">{{$claimed?'Claimed':'Posted'}} by {{$task->creator->getShortName()}}</div>
 </div>
