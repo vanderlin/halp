4faffb360df96d0c52f75717c4323df4c195f305
Todd Vanderlin <vanderlin@bo-m-tvanderlin.ideo.com>
2015-07-10 12:04:47 -0400

    task form update and card update

--- app/views/site/tasks/card.blade.php
+++ app/views/site/tasks/card.blade.php
@@ -3,7 +3,7 @@
 		<span class="task-name">{{$task->title}}</span>
 		<hr>
 		<span class="project-name">For {{$task->project->title}}</span>
-		<span class="date">{{$task->duration}}</span>
+		<span class="duration">{{$task->duration}}</span>
 		<span class="date">{{$task->created_at->toFormattedDateString()}}</span>
 		@if (!$claimed)
 			<div class="progress-button small">
