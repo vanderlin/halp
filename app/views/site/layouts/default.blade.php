c1d121a848c4d55bece34c2a32835880f879657f
Todd Vanderlin <vanderlin@bo-m-tvanderlin.local>
2015-07-09 20:09:39 -0400

    bower and js update: run "bower update"

--- app/views/site/layouts/default.blade.php
+++ app/views/site/layouts/default.blade.php
@@ -23,10 +23,40 @@
     </div>
   
     <!-- Retina JS -->
-    <script src="{{ asset('bower_components/retinajs/dist/retina.min.js') }}"></script>
-    <script src="{{asset('assets/js/classie.js')}}"></script>
-    <script src="{{asset('assets/js/uiProgressButton.js')}}"></script>
-
+    <script src="{{bower('retinajs/dist/retina.min.js') }}"></script>
+  
+    <script src="assets/js/classie.js"></script>
+    <script>
+      (function() {
+        // trim polyfill : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/Trim
+        if (!String.prototype.trim) {
+          (function() {
+            // Make sure we trim BOM and NBSP
+            var rtrim = /^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g;
+            String.prototype.trim = function() {
+              return this.replace(rtrim, '');
+            };
+          })();
+        }
+        [].slice.call( document.querySelectorAll( 'input.input__field' ) ).forEach( function( inputEl ) {
+          // in case the input is already filled..
+          if( inputEl.value.trim() !== '' ) {
+            classie.add( inputEl.parentNode, 'input--filled' );
+          }
+          // events:
+          inputEl.addEventListener( 'focus', onInputFocus );
+          inputEl.addEventListener( 'blur', onInputBlur );
+        } );
+        function onInputFocus( ev ) {
+          classie.add( ev.target.parentNode, 'input--filled' );
+        }
+        function onInputBlur( ev ) {
+          if( ev.target.value.trim() === '' ) {
+            classie.remove( ev.target.parentNode, 'input--filled' );
+          }
+        }
+      })();
+    </script>
 
     @if (isset($use_footer)?$use_footer:true)
       @include('site.layouts.footer')
