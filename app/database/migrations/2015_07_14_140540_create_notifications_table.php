461e2192498d712dd07313a683ece4c75f6e140e
Todd Vanderlin <vanderlin@bo-m-tvanderlin.ideo.com>
2015-07-14 12:04:26 -0400

    notifications model added - you need to run: art site:setup --reset=true --seed=true

---
+++ app/database/migrations/2015_07_14_140540_create_notifications_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateNotificationsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('notifications', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->integer('task_id');
+			$table->string('event');
+			$table->timestamp('sent_on')->nullable()->default(NULL);
+			$table->timestamps();
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('notifications');
+	}
+
+}
---
+++ app/views/admin/roles/edit-user-roles.blade.php
@@ -0,0 +1,62 @@
+@extends('admin.layouts.default', ['use_footer'=>false])
+
+{{-- Web site Title --}}
+@section('title')
+  {{Config::get('config.site_name')}} | Admin | Edit Roles
+@stop
+
+@section('scripts')
+
+@stop
+
+
+
+{{-- Content --}}
+@section('content')
+
+
+<section class="admin">
+    
+    <div class="ui text container" style="text-align: left;">
+        <div class="ui breadcrumb">
+            <a href="{{URL::to('admin')}}" class="section">Admin</a>
+                <div class="divider"> / </div>
+            <a href="{{URL::to('admin/users')}}" class="section">Users</a>
+            <div class="divider"> / </div>
+            <div class="active section">{{$user->getName()}}</div>
+        </div>
+    </div>
+    <br>
+
+    <div class="ui text container" style="text-align: left">
+
+        {{Form::open(['url'=>URL::to('admin/users/'.$user->id), 'method'=>'PUT', 'class'=>'ui form'])}}
+        <div class="field">
+            <label for="username">Username</label>
+            <input type="text" id="username" placeholder="Username" value="{{$user->username}}" disabled>
+        </div>
+
+        <div class="field">
+          <label for="email">Email</label>
+            <input type="email" id="email" placeholder="example@ideo.com" value="{{$user->email}}" disabled>
+        </div>
+     
+
+        <div class="field">
+            @include('admin.partials.roles', ['user'=>$user])
+        </div>
+        
+        <button type="submit" class="ui button">Update</button> {{ link_to($user->getURL(), 'View Profile', ['class'=>'']) }}
+
+      </div>
+
+    {{Form::close()}}
+
+    <div class="container text">
+            @include('site.partials.form-errors')
+    </div>
+
+  </div>
+</section>
+
+@stop
