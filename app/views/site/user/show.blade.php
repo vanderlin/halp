214c1407955ddb98bc08087043a63d30f83647e0
Kim Miller <kjmiller1029@gmail.com>
2015-07-14 10:05:43 -0400

    CSS for claim task modal

--- app/views/site/user/show.blade.php
+++ app/views/site/user/show.blade.php
@@ -18,7 +18,7 @@
 		<h3>{{$user->getName()}}</h3>
 		<hr>
 		<p>E-mail me when a new task is added to Halp:</p>
-		<a href="#yes"><img src="{{img('circle_check.svg')}}"></a>
+		<a href="#yes"><img class="check-box" src="{{img('circle_check.svg')}}"></a>
 		<h5>Yes Please</h5>
 	</section>
 
@@ -29,7 +29,7 @@
 		@forelse ($user->claimedTasks as $task)
 			@include('site.tasks.claimed-task', array('task' => $task))
 		@empty
-			<h5>No Completed Tasks</h5>
+			<br /><h5>It's time to start helping.</h5>
 		@endforelse
 	</section>
 @stop
