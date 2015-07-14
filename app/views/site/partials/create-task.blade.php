bae96db5a5de61b7e9791ee96c61b00147cb4bac
Kim Miller <kjmiller1029@gmail.com>
2015-07-14 14:04:59 -0400

    More typography tweaks

--- app/views/site/partials/create-task.blade.php
+++ app/views/site/partials/create-task.blade.php
@@ -30,7 +30,7 @@
 	<div class="box">
 		<!-- progress button -->
 		<div class="progress-button elastic">
-			<button><span>Halp Me</span></button>
+			<button><span>Help Me</span></button>
 			<svg class="progress-circle" width="70" height="70"><path d="m35,2.5c17.955803,0 32.5,14.544199 32.5,32.5c0,17.955803 -14.544197,32.5 -32.5,32.5c-17.955803,0 -32.5,-14.544197 -32.5,-32.5c0,-17.955801 14.544197,-32.5 32.5,-32.5z"/></svg>
 			<svg class="checkmark" width="70" height="70"><path d="m31.5,46.5l15.3,-23.2"/><path d="m31.5,46.5l-8.5,-7.1"/></svg>
 			<svg class="cross" width="70" height="70"><path d="m35,35l-9.3,-9.3"/><path d="m35,35l9.3,9.3"/><path d="m35,35l-9.3,9.3"/><path d="m35,35l9.3,-9.3"/></svg>
