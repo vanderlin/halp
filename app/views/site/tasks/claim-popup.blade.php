df2278348cf005f90f5fa9066a74bd5ba5839e76
Todd Vanderlin <vanderlin@bo-m-tvanderlin.ideo.com>
2015-07-15 16:54:46 -0400

    user permissions for task,profiles

--- app/views/site/tasks/claim-popup.blade.php
+++ app/views/site/tasks/claim-popup.blade.php
@@ -1,75 +1,19 @@
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0;">
-<title>Notify</title>
-<style type="text/css">
-div, p, a, li, td { -webkit-text-size-adjust:none; }
-.ReadMsgBody{width: 100%; background-color: #f3f3f3;}
-.ExternalClass{width: 100%; background-color: #f3f3f3;}
-body{width: 100%; height: 100%; background-color: #f3f3f3; margin:0; padding:0; -webkit-font-smoothing: antialiased;}
-html{width: 100%;}
+<img src="{{asset('assets/img/front-facing-turtle.png')}}" width="132px" height="64px" class="front-facing-turtle"/>
 
-@font-face {font-family: 'proxima_nova_softmedium';src: url('http://www.rocketway.net/themebuilder/demo_template/folio/font/mark_simonson_-_proxima_nova_soft_medium-webfont.eot');src: url('http://www.rocketway.net/themebuilder/demo_template/folio/font/mark_simonson_-_proxima_nova_soft_medium-webfont.eot?#iefix') format('embedded-opentype'),url('http://www.rocketway.net/themebuilder/demo_template/folio/font/mark_simonson_-_proxima_nova_soft_medium-webfont.woff') format('woff'),url('http://www.rocketway.net/themebuilder/demo_template/folio/font/mark_simonson_-_proxima_nova_soft_medium-webfont.ttf') format('truetype');font-weight: normal;font-style: normal;
-}
 
-@font-face {font-family: 'proxima_nova_softregular';src: url('http://www.rocketway.net/themebuilder/demo_template/folio/font/mark_simonson_-_proxima_nova_soft_regular-webfont.eot'); src: url('http://www.rocketway.net/themebuilder/demo_template/folio/font/mark_simonson_-_proxima_nova_soft_regular-webfont.eot?#iefix') format('embedded-opentype'),url('http://www.rocketway.net/themebuilder/demo_template/folio/font/mark_simonson_-_proxima_nova_soft_regular-webfont.woff') format('woff'),url('http://www.rocketway.net/themebuilder/demo_template/folio/font/mark_simonson_-_proxima_nova_soft_regular-webfont.ttf') format('truetype');font-weight: normal;font-style: normal;
-}
-
-.hover:hover {opacity:0.90;filter:alpha(opacity=90);}
-
-</style>
-
-<table width="100%" border="0" cellpadding="0" cellspacing="0" align="center">
-	<tr>
-		<td>
-		
-			<table width="960" border="0" cellpadding="0" cellspacing="0" align="center" style="margin-top: 50px; margin-bottom: 100px;">
-				<tr>
-					<td width="960">
-						
-						<table width="960" border="0" cellpadding="0" cellspacing="0" align="center">
-							<tr>
-								<td width="960" bgcolor="#ffffff" style="border: 1px solid #e7eeee; border-radius: 5px;">
-									<table width="960" border="0" cellpadding="0" cellspacing="0" align="center" style="margin-top: 40px;">
-										<tr>
-											<td width="960" style="padding-bottom: 40px; border-bottom: 1px solid #e7eeee;">
-												<center><img src="http://www.rocketway.net/themebuilder/demo_template/panorama/images/logo.png" alt="RocketWay" border="0"></center>
-											</td>
-										</tr>
-										<tr>
-											<td width="960" style="font-size: 39px; color: #65707a; text-align: center; font-family: 'proxima_nova_softmedium', Helvetica, Arial, sans-serif; line-height: 48px; padding-top: 40px;">
-											Thank you for trying out our demo ThemeBuilder!
-											</td>
-										</tr>
-									</table>
-									<table width="960" border="0" cellpadding="0" cellspacing="0" align="center" style="margin-top: 60px; margin-bottom: 60px;">
-										<tr>
-											<td width="40"></td>
-											<td width="370" style="text-align: center;" valign="top">
-												<p style="font-size: 16px; color: #686868; text-align: center; font-family: 'proxima_nova_softmedium', Helvetica, Arial, sans-serif; line-height: 24px;">Once purchased this theme you’ll receive a link to our themebuilder so you can fully customize your newsletter. Click on the link and start building!</p><p style="margin-bottom: 28px;"></p><center><img src="http://www.rocketway.net/themebuilder/demo_template/panorama/images/arrow.jpg" alt="" border="0"></center><p style="margin-bottom: 5px;"></p><br/><br/><a href="http://themeforest.net/item/-notify-notification-email-themebuilder-access/8143225?WT.ac=category_item&WT.z_author=RocketWay" target="_blank" style="background-color: #65707a; font-family: 'proxima_nova_softmedium', Helvetica, Arial, sans-serif; text-decoration: none; color: #ffffff; padding: 10px 20px 10px 20px; border-radius: 4px; font-size: 18px;" class="hover">Purchase this template</a><p style="margin-bottom: 5px;"><br/></p><a href="http://themeforest.net/user/RocketWay/portfolio" target="_blank" style="background-color: #51c4d4; font-family: 'proxima_nova_softmedium', Helvetica, Arial, sans-serif; text-decoration: none; color: #ffffff; padding: 10px 20px 10px 20px; border-radius: 4px; font-size: 18px;" class="hover">Browse other templates</a>
-											</td>
-											<td width="546"><center><img src="http://www.rocketway.net/themebuilder/demo_template/notify/images/template.jpg" alt="" border="0"></center><center><img src="http://www.rocketway.net/themebuilder/demo_template/panorama/images/shadow.jpg" alt="" border="0"></center></td>
-											<td width="4"></td>
-										</tr>
-									</table>
-									<table width="960" border="0" cellpadding="0" cellspacing="0" align="center" bgcolor="#65707a">
-										<tr>
-											<td width="550" height="100" style="font-size: 16px; color: #ffffff; text-align: right; font-family: 'proxima_nova_softregular', Helvetica, Arial, sans-serif; line-height: 24px; padding-right: 80px;">
-											Thank you for your support! Team RocketWay 
-											</td>
-											<td width="408" height="100" style="font-size: 16px; color: #ffffff; text-align: left; font-family: 'proxima_nova_softregular', Helvetica, Arial, sans-serif; line-height: 24px;">
-											<a href="mailto:support@rocketway.net" style="color: #ffffff;">support@rocketway.net</a>
-											</td>
-										</tr>
-									</table>
-								</td>
-							</tr>
-						</table>
-
-					</td>
-				</tr>
-			</table>
-			
-		</td>
-	</tr>
-</table>
-
+<div class="white-popup claimed-popup">
+	<h2>Help {{$task->creator->getShortName()}} with {{ucfirst($task->title)}}</h2>
+	<hr>
+	<div class="task-message">
+		<p>
+			This task is for {{link_to($task->project->getURL(), $task->project->title)}}. 
+			{{ucfirst($task->creator->firstname)}} estimates this task will take about {{$task->duration}}.
+		</p>
+	</div>
+	<div class="progress-button small claimed-buttons">
+		{{Form::open(['route'=>['tasks.claim', $task->id]])}}
+			<button data-id="{{$task->id}}"><span>Send E-Mail</span></button>
+			<button data-id="{{$task->id}}"><span>Talk IRL</span></button>
+		{{Form::close()}}
+	</div>
+</div>
