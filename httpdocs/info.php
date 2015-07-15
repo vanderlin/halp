c60d9ad3f67e8969955ad6511ea0747ff05566d5
halp <halp@p-lin-app-df-02.ideo.com>
2015-07-15 18:18:24 +0000

    rackspace suck it!

--- httpdocs/info.php
+++ httpdocs/info.php
@@ -1,9 +1,20 @@
+<?php opcache_reset(); ?>
+<pre>
+
 <?php
 
+print_r(
 
-print_r($_SERVER['SERVER_NAME']);
-print_r($_SERVER['SERVER_PORT']);
+	[	
+		'this'=>dirname(__FILE__),
+		['file'=>__DIR__.'/vars.php', 'data'=>require_once __DIR__.'/vars.php'],
+		['file'=>__DIR__.'/../sub.php', 'data'=>require_once __DIR__.'/../sub.php'],
+		['file'=>__DIR__.'/../bootstrap/paths.php', 'data'=>require_once __DIR__.'/../bootstrap/paths.php'],
+		['file'=>__DIR__.'/../bootstrap/boot.php', 'data'=>require_once __DIR__.'/../bootstrap/boot.php'],
+		'$_SERVER'=>$_SERVER,
+	]
+	);
+
 ?>
 
-
-<h1>TEST</h1>
+</pre>
