a5d3e592f9ba1bf46cd2404b29a5f71113652c42
Todd Vanderlin <vanderlin@bo-m-tvanderlin.ideo.com>
2015-07-17 10:18:44 -0400

    hiding public credientails ooops

--- app/config/database.php
+++ app/config/database.php
@@ -54,10 +54,10 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => 'host',
-			'database'  => 'dev',
-			'username'  => 'user',
-			'password'  => 'password',
+			'host'      => 'localhost',
+			'database'  => 'forge',
+			'username'  => 'forge',
+			'password'  => '',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
