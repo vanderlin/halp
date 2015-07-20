559ac0f728128b9b9981172ab53e736e025413f2
Todd Vanderlin <vanderlin@bo-m-tvanderlin.ideo.com>
2015-07-20 13:45:54 -0400

    email button fix

--- app/views/emails/task-claimed.blade.php
+++ app/views/emails/task-claimed.blade.php
@@ -15,7 +15,7 @@
 				<h2>{{link_to($task->claimer->getProfileURL(), $task->claimer->getShortName())}} has claimed one of your tasks!</h2>
 				<hr>
 				<h3>You asked for help with:</h3>
-				<h1>{{link_to($task->getURL(), $task->title)}} for {{link_to($task->project->getURL(), $task->project->title)}}</h1>
+				<h1>{{link_to($task->getURL(), $task->title.' for '.$task->project->title)}}</h1>
 				<p>You estimated this task would take {{$task->duration}}. Go talk to {{link_to($task->claimer->getURL(), $task->claimer->firstname)}} and happy task-ing!</p>
 			</div>
 		</div>
