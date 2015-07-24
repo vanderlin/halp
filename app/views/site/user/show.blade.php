00295d80dc95975be7edb113c13f841cb0842e48
Todd Vanderlin <vanderlin@bo-m-tvanderlin.ideo.com>
2015-07-24 13:21:04 -0400

    logic to show details or not on a users page for tasks they created

--- app/views/site/user/show.blade.php
+++ app/views/site/user/show.blade.php
@@ -89,7 +89,7 @@
 			<h2>{{$user->totalCreated()}} Task{{$user->totalCreated()>1?'s':''}} Created!</h2>
 		</div>
 		@forelse ($user->createdTasks as $task)
-			@include('site.tasks.card', array('task' => $task, 'class'=>'user-created-card'))
+			@include('site.tasks.card', array('task' => $task, 'class'=>'user-created-card', 'show_button'=>$task->isMine()?true:($task->isClaimed?false:true)))
 		@empty
 			<br/><h5>Go make some {{link_to('/', 'tasks')}}</h5>
 		@endforelse
