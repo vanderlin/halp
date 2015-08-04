58c8710ba1035e676def1e90a5ae8f84f2698d6f
Kim Miller <kjmiller1029@gmail.com>
2015-08-04 09:01:58 -0400

    404 page

--- app/views/site/pages/404.blade.php
+++ app/views/site/pages/404.blade.php
@@ -1,1 +1,22 @@
-/Users/tvanderlin/IDEO/Dev/halp/app/views/site/pages/404.blade.php
+@extends('site.layouts.default', ['use_navigation'=>false, 'use_footer'=>false])
+
+{{-- Web site Title --}}
+@section('title')
+{{Config::get('config.site_name')}} | 404 
+@stop
+
+@section('content')
+		
+	<section>
+		<div class="error-404">
+			<h1>404</h1>
+			<h2>Lost your shell?</h2>
+			<h3>The page you're looing for couldn't be found.</h3>
+			<div class="progress-button">
+				<a href="{{URL::to('/')}}"><button><span>Go Home</span></button></a>
+			</div>
+			<img src="{{asset('assets/img/naked-turtle.svg')}}">
+		</div>
+	</section>
+@stop
+    
